98c94022c78604eb0c8f2a5c36ca7d08e0270982
rure00 <asnjkd@naver.com>
2025-01-20 18:53:02 +0900

    fix: 로그인화면 textField 편집 시 화면 올려주기

--- app/src/main/java/com/rure/angkorlife/presentation/MainActivity.kt
+++ app/src/main/java/com/rure/angkorlife/presentation/MainActivity.kt
@@ -1,11 +1,9 @@
-package com.rure.angkorlife
+package com.rure.angkorlife.presentation
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.imePadding
@@ -14,29 +12,20 @@ import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
-import com.rure.angkorlife.domain.repository.RetrofitRepository
-import com.rure.angkorlife.domain.repository.RetrofitResult
 import com.rure.angkorlife.presentation.component.AppTopBar
 import com.rure.angkorlife.presentation.navigation.Destination
 import com.rure.angkorlife.presentation.navigation.MainNavRoute
 import com.rure.angkorlife.presentation.navigation.mainNavGraph
-import com.rure.angkorlife.presentation.viewmodel.MainViewModel
 import com.rure.angkorlife.ui.theme.AngkorLifeTheme
 import com.rure.angkorlife.ui.theme.BackgroundBlack
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/rure/angkorlife/presentation/screen/LoginScreen.kt
+++ app/src/main/java/com/rure/angkorlife/presentation/screen/LoginScreen.kt
@@ -1,19 +1,33 @@
 package com.rure.angkorlife.presentation.screen
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -25,6 +39,7 @@ import com.rure.angkorlife.presentation.viewmodel.MainViewModel
 import com.rure.angkorlife.ui.theme.BackgroundBlack
 import com.rure.angkorlife.ui.theme.ButtonBlue
 import com.rure.angkorlife.ui.theme.TextGray
+import kotlinx.coroutines.launch
 
 @Composable
 fun LoginScreen(
@@ -35,10 +50,22 @@ fun LoginScreen(
 
     val idState = remember { mutableStateOf("") }
 
+    val scrollState = rememberScrollState()
+    val coroutineScope = rememberCoroutineScope()
+    val homeBottomImageHeight = remember {
+        mutableIntStateOf(0)
+    }
+    val keyboardHeight = WindowInsets.ime.getBottom(LocalDensity.current)
+
+    LaunchedEffect(key1 = keyboardHeight) {
+        coroutineScope.launch {
+            scrollState.scrollBy(keyboardHeight.toFloat())
+        }
+    }
 
 
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize().verticalScroll(scrollState)
     ) {
         Image(
             painter = painterResource(R.drawable.home_top),
